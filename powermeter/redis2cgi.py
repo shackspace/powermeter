0fb2419cda041347726809d84cc8b19bd7e2030e
makefu <github@syntax-fehler.de>
2019-07-17 00:54:09 +0200

    add legacy files

---
+++ powermeter/redis2cgi.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python
+#
+# get the latest power consumption values from redis in JSON format
+# pass parameter ?n=<number> to control the number of items returned
+#
+# {"Total": [3734, ...], "Minutes ago": [0, ...], "L1.Power": [1612, ...], "L3.Power": [1363, ...], "L2.Power": [759, ...]}
+#
+
+import redis
+import json
+import cgi
+
+getp = cgi.FieldStorage();
+
+baseKey = "sensordata.shackspace.20745965.data."
+sensors = ["L1.Power","L2.Power","L3.Power"]
+numValues = 500
+
+
+if "n" in getp:
+        numValues = int(getp["n"].value)
+
+rc = redis.Redis("glados.shack")
+
+# ensure all queries are atomic so the time values are sync
+pipe = rc.pipeline(transaction=True)
+
+for sensor in sensors:
+        # get the numValues latest values
+        pipe.lrange(baseKey+sensor, -numValues, -1)
+
+res = pipe.execute()
+
+output = {}
+
+# convert time on the fly
+latest = json.loads(res[0][-1])[0]
+output["Minutes ago"] = [(json.loads(kv)[0]-latest)/60000.0 for kv in res[0]]
+
+for idx, sensordata in enumerate(res):
+        output[sensors[idx]] = [json.loads(kv)[1] for kv in sensordata]
+
+from operator import add
+output["Total"] = list(map(     add, list(map(  add, output[sensors[0]], output[sensors[1]])), output[sensors[2]] ))
+
+# off we go
+print("Content-Type: application/json")
+print()
+print(json.dumps(output))
---
+++ powermeter/socket2redis.py
@@ -0,0 +1,111 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+import socket
+import sys
+import re
+import time
+import redis
+
+time.sleep(10)
+
+def storeSensorValueInRedis(pipe, meterSerial, timestamp, channelName, value):
+    "stores a sensor reading in redis."
+    baseKey = "sensordata.shackspace." + meterSerial + ".data." + channelName
+    value = "[" + str(1000*int(timestamp)) + "," + re.sub("^0*", "", value) + "]";
+    pipe.rpush(baseKey, value)
+    return
+
+def storeSensorConfigurationInRedis(meterSerial):
+    "stores the sensor configuration in redis."
+    baseKey = "sensordata.shackspace." + meterSerial + ".config.sensors"
+    value = "{\"L1.Voltage\": {\"unit\": \"V\", \"type\":\"actual\"},\"L2.Voltage\": {\"unit\": \"V\", \"type\":\"actual\"},\"L3.Voltage\": {\"unit\": \"V\", \"type\":\"actual\"},\"L1.Current\": {\"unit\": \"A\", \"type\":\"actual\"},\"L2.Current\": {\"unit\": \"A\", \"type\":\"actual\"},\"L3.Current\": {\"unit\": \"A\", \"type\":\"actual\"},\"L1.Power\": {\"unit\": \"W\", \"type\":\"actual\"},\"L2.Power\": {\"unit\": \"W\", \"type\":\"actual\"},\"L3.Power\": {\"unit\": \"W\", \"type\":\"actual\"},\"Total\": {\"unit\": \"W\", \"type\":\"cummulative\"}}";
+
+    redisConnection.set(baseKey, value)
+    return
+
+def readPowerMeterValue(server_adress):
+    # Connect the socket to the port where the server is listening
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+       sock.connect(server_address)
+    except socket.error:
+       sys.exit(1)
+    
+    try:
+      # Look for the response
+      amount_received = 0
+      amount_expected = 23 # dirty hack but its okay since the powerraw device will close the connection for us
+      data_received = ""
+      while amount_received < amount_expected:
+          #data = sock.recv(25000)
+          data = sock.recv(1024)
+          amount_received += len(data)
+          data_received += data
+      #    print >>sys.stderr, 'received "%s"' % data
+    finally:
+        sock.close()
+    
+    return data_received;
+
+
+# Create a TCP/IP socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# connect to redis.
+redisConnection = redis.Redis("glados.shack")
+
+# define regex for parsing.
+regexPower = re.compile("[+][0-9]+[*]")
+regexCurrent = re.compile("[0-9.]+[*]A")
+regexVoltage = re.compile("[0-9.]+[*]V")
+regexReading = re.compile("1-0:1\.8\..\*255\(([0-9.]+)\)")
+regexSerial = re.compile("1-0:0\.0\.0\*255\(([0-9]+)\)")
+regexEpochTime = re.compile("[0-9]+")
+server_address = ('powerraw.shack', 11111)
+epochtime_old = 0
+
+# read one sample to extract the serial of the 
+data_received = readPowerMeterValue(server_address)
+meterId = regexSerial.search(data_received).group(1)
+storeSensorConfigurationInRedis(meterId);
+
+# read the redis value
+while True:
+    # read a data sample.
+    data_received = readPowerMeterValue(server_address)
+
+    # check if it's a new value
+    epochTime = regexEpochTime.findall(data_received)
+    if epochtime_old != epochTime[0]:
+      epochtime_old = epochTime[0]
+      currents = regexCurrent.findall(data_received)
+      voltages = regexVoltage.findall(data_received)
+      powerUsage = regexPower.findall(data_received)
+      totalReading = regexReading.search(data_received).groups()
+
+      # create a json object with the data.
+      try:
+        pipe = redisConnection.pipeline(transaction=True)
+        for i in range(0,3):
+          storeSensorValueInRedis(pipe, meterId, epochTime[0], "L"+str(1+i)+".Voltage", voltages[i].strip("*V"));
+          storeSensorValueInRedis(pipe, meterId, epochTime[0], "L"+str(1+i)+".Current", currents[i].strip("*A"));
+          storeSensorValueInRedis(pipe, meterId, epochTime[0], "L"+str(1+i)+".Power",   powerUsage[i].strip("+*"));
+          
+        storeSensorValueInRedis(pipe, meterId, epochTime[0], "Total", totalReading[0]);
+        pipe.execute()
+      except: 
+        print("Error contacting redis. Trying to reconnect.")
+        redisConnection = redis.Redis("glados.shack")
+
+      # TODO: Update redis for every phase
+      print(("Meter   : " + meterId)) 
+      print(("Time    : " + epochTime[0]))
+      print(("Voltage : " + voltages[0].strip("*V") + " / " + voltages[1].strip("*V") + " / " + voltages[2].strip("*V")))
+      print(("Current : " + currents[0].strip("*A") + " / " + currents[1].strip("*A") + " / " + currents[2].strip("*A")))
+      print(("Power   : " + powerUsage[0].strip("+*") + " / " + powerUsage[1].strip("+*") + " / " + powerUsage[2].strip("+*")))
+      print(("Consumed: " + totalReading[0]))
+      print("==")
+    else:
+          print(("not querying, no update: " + epochTime[0])) 
+    time.sleep(1)
+
